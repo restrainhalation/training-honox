2a131104059ee8de85532492800733a724ea2ecb
restrainhalation <168318744+restrainhalation@users.noreply.github.com>
2024-06-17 21:40:57 +0900

    📘 chore: install mdx

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 // vite.config.ts
+import mdx from '@mdx-js/rollup';
 import ssg from '@hono/vite-ssg'
 import honox from 'honox/vite'
 import client from 'honox/vite/client'
+import remarkFrontmatter from 'remark-frontmatter';
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
 import { defineConfig } from 'vite'
 
 const entry = './app/server.ts'
@@ -16,7 +19,14 @@ export default defineConfig(({ mode }) => {
       build: {
         emptyOutDir: false,
       },
-      plugins: [honox(), ssg({ entry })],
+      plugins: [
+        honox(),
+        ssg({ entry }),
+        mdx({
+          jsxImportSource: 'hono/jsx',
+          remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+        })
+      ],
     }
   }
 })
