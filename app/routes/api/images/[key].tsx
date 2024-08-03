fe3cbaa5743d4ee5f1d78cbbf69592add7e5c809
restrainhalation <168318744+restrainhalation@users.noreply.github.com>
2024-08-03 09:47:44 +0900

    📝 feat: add log to api for get image

--- app/routes/api/images/[key].tsx
+++ app/routes/api/images/[key].tsx
@@ -13,21 +13,25 @@ export default createRoute(async (c) => {
     if (value && metadata) {
       return c.body(value, 200, metadata.headers)
     }
+    console.log('Not found (KV)')
 
     const bucket = c.env.R2
     const { key } = c.req.param()
     const bucketObject = await bucket.get(key)
     if (bucketObject !== null) {
     const { body, httpMetadata, etag } = bucketObject
-      await c.env.KV.put(c.req.url, body, {
-        metadata: httpMetadata,
-      })
-      return c.body(body, 200, {
-        'Cache-Control': `public, max-age=${60 * 60 * 24 * 30}`,
-        'Content-Type': httpMetadata?.contentType ?? 'application/octet-stream',
-        'etag': `"${etag}"`
-      })
+    console.log('R2: ', body, httpMetadata, etag)
+    await c.env.KV.put(c.req.url, body, {
+      metadata: httpMetadata,
+    })
+    console.log('PUT to R2')
+    return c.body(body, 200, {
+      'Cache-Control': `public, max-age=${60 * 60 * 24 * 30}`,
+      'Content-Type': httpMetadata?.contentType ?? 'application/octet-stream',
+      'etag': `"${etag}"`
+    })
     } else {
+      console.log('Not found (R2)')
       return c.notFound()
     }
 
