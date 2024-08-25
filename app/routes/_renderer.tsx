f4f032515b38b92c84faab977460f47691058bf7
restrainhalation <168318744+restrainhalation@users.noreply.github.com>
2024-08-25 15:56:14 +0900

    🏝️ feat: create component of has islands

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -1,5 +1,12 @@
-import { reactRenderer } from '@hono/react-renderer'
+import { reactRenderer, useRequestContext } from '@hono/react-renderer'
 import { UIProvider } from '@yamada-ui/react'
+import { FC, PropsWithChildren } from 'react';
+
+const HasIslands: FC<PropsWithChildren> = ({ children }) => {
+  const IMPORTING_ISLANDS_ID = "__importing_islands" as const;
+  const c = useRequestContext();
+  return <>{c.get(IMPORTING_ISLANDS_ID) ? children : <></>}</>;
+};
 
 export default reactRenderer(({ children, title }) => {
   return (
@@ -8,7 +15,9 @@ export default reactRenderer(({ children, title }) => {
         <meta charSet='UTF-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         {import.meta.env.PROD ? (
-          <script type='module' src='/static/client.js'></script>
+          <HasIslands>
+            <script type="module" src="/static/client.js"></script>
+          </HasIslands>
         ) : (
           <script type='module' src='/app/client.ts'></script>
         )}
